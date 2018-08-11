10c473b07de34760f05772e1d2469482d3d97beb
Jake Treacher <git@jaketreacher.com>
2018-08-11 18:33:27 +0800

    more changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,8 +9,8 @@ MODE="$1"
 if [ -z $MODE ]; then exit 1; fi
 
 # Env Vars
-if [ -z $USER ]; then exit 1; fi
-if [ -z $ADDRESS ]; then exit 1; fi
+if [ -z $REMOTEUSER ]; then exit 1; fi
+if [ -z $REMOTEADDRESS ]; then exit 1; fi
 
 # SSH Key
 if [ ! -f "travis_deploy" ]; then exit 1; fi
@@ -20,9 +20,9 @@ if [ ! -f "travis_deploy" ]; then exit 1; fi
 #----------------------------------------------------------#
 
 if [ "$MODE" == "production" ]; then
-    ssh -i travis_deploy $USER@$ADDRESS 'bash -s' < scripts/production.sh
+    ssh -i travis_deploy $REMOTEUSER@$REMOTEADDRESS 'bash -s' < scripts/production.sh
 elif [ "$MODE" == "staging" ]; then
-    ssh -i travis_deploy $USER@$ADDRESS 'bash -s' < scripts/staging.sh
+    ssh -i travis_deploy $USER@$REMOTEADDRESS 'bash -s' < scripts/staging.sh
 else
     echo "No mode selected."
     exit 1
