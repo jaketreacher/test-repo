e0c2ec591c99c60c1605b3de4a7de64151499b9b
Jake Treacher <git@jaketreacher.com>
2018-08-12 08:01:24 +0800

    remove ssh check

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,19 +12,16 @@ if [ -z $MODE ]; then echo "Bad Mode"; exit 1; fi
 if [ -z $REMOTEUSER ]; then echo "No User"; exit 1; fi
 if [ -z $REMOTEADDRESS ]; then echo "No Address"; exit 1; fi
 
-# SSH Key
-if [ ! -f "travis_deploy" ]; then exit 1; fi
-
 #----------------------------------------------------------#
 #                       Action                             #
 #----------------------------------------------------------#
 
 if [ "$MODE" == "production" ]; then
     echo "Deploying: production"
-    # ssh -i ~/.ssh/id_rsa $REMOTEUSER@$REMOTEADDRESS 'bash -s' < scripts/production.sh
+    ssh -i ~/.ssh/id_rsa $REMOTEUSER@$REMOTEADDRESS 'bash -s' < scripts/production.sh
 elif [ "$MODE" == "staging" ]; then
     echo "Deploying: staging"
-    # ssh -i ~/.ssh/id_rsa $REMOTEUSER@$REMOTEADDRESS 'bash -s' < scripts/staging.sh
+    ssh -i ~/.ssh/id_rsa $REMOTEUSER@$REMOTEADDRESS 'bash -s' < scripts/staging.sh
 else
     echo "No mode selected."
     exit 1
